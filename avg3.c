3ecacaa504549b234ead6faa911aa737eadfe9e2
Vishva-sc <desaivishva2023@gmail.com>
2026-08-01 18:39:24 +0530

    average of 3 numbers

---
+++ avg3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,avg;
+    printf("Enter value of a:");
+    scanf("%d",&a);
+    printf("Enter value of b:");
+    scanf("%d",&b);
+    printf("Enter value of c:");
+    scanf("%d",&c);
+    avg=(a+b+c)/3;
+    printf("Average of %d, %d and %d is: %d",a,b,c,avg);    
+    return 0;
+}
