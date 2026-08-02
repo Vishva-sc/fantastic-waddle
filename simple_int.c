619fdc075e8e630d90c51533615aa97442a8b956
Vishva-sc <desaivishva2023@gmail.com>
2026-08-02 15:37:39 +0530

    calculating simple interest

---
+++ simple_int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int P,R,N;
+    float SI;
+    printf("Enter the Principal Amount: ");
+    scanf("%d",&P);
+    printf("Enter the Rate of Interest: ");
+    scanf("%d",&R);
+    printf("Enter the Time Period in years: ");
+    scanf("%d",&N);
+    SI = (P * R * N) / 100;
+    printf("The Simple Interest is: %f", SI);
+    return 0;
+}
