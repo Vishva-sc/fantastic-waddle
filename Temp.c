878036c7137eadac0d106a5f9a4688acc70e72f8
Vishva-sc <desaivishva2023@gmail.com>
2026-08-02 15:45:32 +0530

    temp conversion (F->C)

---
+++ Temp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float F, C;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &F);
+    C = (F - 32) * 5 / 9;
+    printf("Temperature in Celsius: %.2f\n", C);
+    return 0;
+}
